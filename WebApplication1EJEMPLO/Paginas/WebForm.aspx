b58778fc365eaef1c92da23177b8a7c9ea33fe87
Dvb0312 <Usuario@LAPTOP-P81C32JE>
2025-05-25 20:18:03 -0600

    edad validacion terminado

---
+++ WebApplication1EJEMPLO/Paginas/WebForm.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="WebApplication1EJEMPLO.Paginas.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           
+           <h2> formulario para validar edad</h2>
+            
+            <label for="txtNombre"> Nombre :</label> 
+            <asp:TextBox ID="txtNombre" runat="server" required="true"> </asp:TextBox>
+            
+            <br /> <br />
+
+            <label for="txtEdad"> Edad :</label> 
+            <asp:TextBox ID="txtEdad" runat="server" required="true"> </asp:TextBox>
+            <br /> <br />
+            <asp:Button ID="btnEnviar" runat="server" Text="Enviar" OnClick="btnEnviar_Click" />
+
+            <asp:Label ID="lblMensaje" runat="server" required="true"> </asp:Label>
+
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1EJEMPLO/Paginas/WebForm.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1EJEMPLO.Paginas
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            int edad;
+
+
+            if (int.TryParse(txtEdad.Text, out edad))
+
+            {
+                lblMensaje.Text = "se connvirtio la edad";
+                if (edad < 0)
+                {
+                    lblMensaje.Text += " edad no valida";
+                    lblMensaje.ForeColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    if (edad < 18)
+                    {
+                        lblMensaje.Text = " todavia no cumples la mayoria de edad";
+                        lblMensaje.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        lblMensaje.Text = " tienes acceso";
+                        lblMensaje.ForeColor = System.Drawing.Color.Green;
+                    }
+                }
+                
+            }
+            else
+            {
+
+                lblMensaje.Text = " NO se connvirtio la edad";
+            }
+
+
+
+        }
+    }
+}
